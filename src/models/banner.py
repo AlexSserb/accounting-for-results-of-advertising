43f52ba3c00c55ee0699da7ca71e93c7280e2ff9
AlexSerb <aserbin190@gmail.com>
2023-12-20 19:17:20 +0400

    Replacing init with post_init in classes for banners

--- src/models/banner.py
+++ src/models/banner.py
@@ -14,60 +14,23 @@ class Banner:
     company_name: str = ''
     date_start: QDate = QDate()
     date_end: QDate = QDate()
-    _min_showings: int = 0
-    _max_showings: int = 0
-    _showings: list[Showing] = ()
+    min_showings: int = 0
+    max_showings: int = 0
+    showings: list[Showing] = ()
 
-    def __init__(self, id: int, name: str, company_name: str, date_start: QDate,
-                 date_end: QDate, min_showings: int, max_showings: int, showings: list):
-        if date_start.daysTo(date_end) < 0:
+    def __post_init__(self):
+        if self.date_start.daysTo(self.date_end) < 0:
             raise Exception('Incorrect dates were entered.')
-        if min_showings < 0 | max_showings < 0:
+        if self.min_showings < 0 | self.max_showings < 0:
             raise Exception('The number of shows cannot be negative.')
-        if min_showings > max_showings:
+        if self.min_showings > self.max_showings:
             raise Exception('The minimum number of shows cannot be higher than the maximum number of shows.')
-        self.id = id
-        self.name = name
-        self.company_name = company_name
-        self.date_start = date_start
-        self.date_end = date_end
-        self.min_showings = min_showings
-        self.max_showings = max_showings
-        self._showings = showings
-
-    @property
-    def min_showings(self) -> int:
-        return self._min_showings
-
-    @min_showings.setter
-    def min_showings(self, min_showings: int):
-        if min_showings < 0:
-            min_showings = 0
-        self._min_showings = min_showings
-
-    @property
-    def max_showings(self) -> int:
-        return self._max_showings
-
-    @max_showings.setter
-    def max_showings(self, max_showings: int):
-        if max_showings < 0:
-            max_showings = 0
-        self._max_showings = max_showings
-
-    @property
-    def showings(self) -> list[Showing]:
-        return self._showings
-
-    @showings.setter
-    def showings(self, sh: list[Showing]):
-        self._showings = sh
 
     def add_showing(self, showing: Showing):
-        self._showings.append(showing)
+        self.showings.append(showing)
 
     def pop_showing(self, index):
-        showing = self._showings.pop(index)
+        showing = self.showings.pop(index)
         return showing
 
     def date_start_str(self) -> str:
@@ -83,7 +46,7 @@ class Banner:
     # Getting the number of banner impressions for the selected date (default for current date)
     def count_showings_for_date(self, date: QDate = QDate.currentDate()) -> int:
         amount = 0
-        for sh in self._showings:
+        for sh in self.showings:
             if sh.datetime.date() == date:
                 amount += 1
         return amount
@@ -99,43 +62,16 @@ class BannerShortData:
     company_name: str = ''
     date_start: QDate = QDate()
     date_end: QDate = QDate()
-    _min_showings: int = 0
-    _max_showings: int = 0
+    min_showings: int = 0
+    max_showings: int = 0
 
-    def __init__(self, name: str, company_name: str, date_start: QDate,
-                 date_end: QDate, min_showings: int, max_showings: int):
-        if date_start.daysTo(date_end) < 0:
+    def __post_init__(self):
+        if self.date_start.daysTo(self.date_end) < 0:
             raise Exception('Incorrect dates were entered.')
-        if min_showings < 0 | max_showings < 0:
+        if self.min_showings < 0 | self.max_showings < 0:
             raise Exception('The number of shows cannot be negative.')
-        if min_showings > max_showings:
+        if self.min_showings > self.max_showings:
             raise Exception('The minimum number of shows cannot be higher than the maximum number of shows.')
-        self.name = name
-        self.company_name = company_name
-        self.date_start = date_start
-        self.date_end = date_end
-        self.min_showings = min_showings
-        self.max_showings = max_showings
-
-    @property
-    def min_showings(self) -> int:
-        return self._min_showings
-
-    @min_showings.setter
-    def min_showings(self, min_showings: int):
-        if min_showings < 0:
-            min_showings = 0
-        self._min_showings = min_showings
-
-    @property
-    def max_showings(self) -> int:
-        return self._max_showings
-
-    @max_showings.setter
-    def max_showings(self, max_showings: int):
-        if max_showings < 0:
-            max_showings = 0
-        self._max_showings = max_showings
 
     def date_start_str(self) -> str:
         return self.date_start.toString(DATE_FORMAT)
